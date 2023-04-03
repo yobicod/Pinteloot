8c15616c6ca833e39bc7aba93d28860c694478be
visal <yobicod.4u@gmail.com>
2023-04-03 10:17:06 +0700

    Add Nav bar

---
+++ src/components/nav.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./nav.css";
+import { useState } from "react";
+import navBell from "../Images/navBell.png";
+import chat from "../Images/chat.png";
+function Nav() {
+  const [searchTxt, setSearchTxt] = useState("");
+  const inputTxt = (event) => {
+    setSearchTxt(event.target.value);
+  };
+  return (
+    <div>
+      <div className="navContainer">
+        <div className="navBtn nav">
+          <p className="homeTxt">Home</p>
+        </div>
+        <div className="navBtn createBtn nav">
+          <p className="createTxt">Create</p>
+        </div>
+        <input
+          className="searchBox nav"
+          type="text"
+          onChange={inputTxt}
+          value={searchTxt}
+        ></input>
+        <img className="navBellIcon nav" src={navBell} alt="Bell icon"></img>
+        <img className="navChatIcon nav" src={chat} alt="Chat icon"></img>
+      </div>
+    </div>
+  );
+}
+
+export default Nav;
